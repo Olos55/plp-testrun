4750984320afd85ec24196373502ad48c3b35faa
Olos55 <159927147+Olos55@users.noreply.github.com>
2024-03-06 18:54:00 +0300

    Add files via upload

---
+++ user-input.py
@@ -0,0 +1,15 @@
+name=input("what is your name:Eve")
+age=input("how old are you:10")
+location=input("where do you live: Thika")
+print('hello[name]',end='')
+print('you are[age] years old',end='')
+print('and live in[location]')
+
+
+
+
+
+
+
+
+print("hello world")
